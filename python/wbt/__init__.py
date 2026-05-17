76dc6b891440f76517d839a0bbaaa9376f55bedf
zengbin93 <zeng_bin8888@163.com>
2026-05-17 13:01:04 +0800

    feat(wbt): expose 5 migrated czsc APIs at top level

--- python/wbt/__init__.py
+++ python/wbt/__init__.py
@@ -3,13 +3,25 @@ from wbt.backtest import WeightBacktest, backtest
 from wbt.mock import mock_symbol_kline, mock_weights
 from wbt.report import generate_backtest_report
 from wbt.top_drawdowns import top_drawdowns
+from wbt.utils import (
+    cal_trade_price,
+    cal_yearly_days,
+    log_strategy_info,
+    rolling_daily_performance,
+    weights_simple_ensemble,
+)
 
 __all__ = [
     "WeightBacktest",
     "backtest",
+    "cal_trade_price",
+    "cal_yearly_days",
     "daily_performance",
     "generate_backtest_report",
+    "log_strategy_info",
     "mock_symbol_kline",
     "mock_weights",
+    "rolling_daily_performance",
     "top_drawdowns",
+    "weights_simple_ensemble",
 ]
